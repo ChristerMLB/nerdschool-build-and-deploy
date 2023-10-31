f60f1066e74dc842bb9da2586d91aa0f88df85b9
Christer M.L. Bendixen <christer@fortelle.no>
2023-10-31 19:16:03 +0100

    dillet med tiden

--- code/src/main.js
+++ code/src/main.js
@@ -1,11 +1,11 @@
 import {  getSecondsLeftOfYear, getTimeString } from "./clock";
 
 const timeContainer = document.getElementById("time");
-const timeLeft = document.getElementById("seconds-left");
+const timeLeft = document.getElementById("minutes-left");
 
 const render = () => {
   const now = new Date();
-  timeLeft.innerText = getSecondsLeftOfYear(now);
+  timeLeft.innerText = (getSecondsLeftOfYear(now)/60).toFixed(2);
   timeContainer.innerText = getTimeString(now);
 }
 
