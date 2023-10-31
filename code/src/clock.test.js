08d59f9e79332c3680375eaea15fb47c4248584a
Christer M.L. Bendixen <christer@fortelle.no>
2023-10-31 18:41:45 +0100

    lagt til noe som får testen til å feile kanskje

--- code/src/clock.test.js
+++ code/src/clock.test.js
@@ -3,7 +3,7 @@ import { getSecondsLeftOfYear, getTimeString } from "./clock";
 test('getTimeString formats time string correctly', () => {
     const now = new Date(0, 0, 0, 10, 20, 30);
     const timeString = getTimeString(now);
-    expect(timeString).toBe('10:20:30');
+    expect(timeString).toBe('10:20:32');
 });
 
 test('getSecondsLeftOfYear returns the correct amount of seconds', () => {
